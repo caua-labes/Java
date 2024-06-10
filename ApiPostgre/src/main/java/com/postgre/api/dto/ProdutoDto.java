2528a808ce975ea0ae199e232c32810a47728073
caua-labes <caua_labes@estudante.sesisenai.org.br>
2024-06-09 22:13:23 -0300

    Criação das entidades de produto e produto dto

---
+++ ApiPostgre/src/main/java/com/postgre/api/dto/ProdutoDto.java
@@ -0,0 +1,43 @@
+package com.postgre.api.dto;
+
+import com.postgre.api.entidades.Categoria;
+
+public class ProdutoDto {
+	private Long id;
+	private String descricao;
+	private long quantidade;
+	private double valor;
+	private Categoria categoria;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public long getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(long quantidade) {
+		this.quantidade = quantidade;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
---
+++ ApiPostgre/src/main/java/com/postgre/api/entidades/Produto.java
@@ -0,0 +1,62 @@
+package com.postgre.api.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Produtos")
+public class Produto {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+private String descricao;
+private long quantidade;
+private double valor;
+@ManyToOne
+@JoinColumn(name = "categoria_id")
+private Categoria categoria;
+
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getDescricao() {
+	return descricao;
+}
+public void setDescricao(String descricao) {
+	this.descricao = descricao;
+}
+public long getQuantidade() {
+	return quantidade;
+}
+public void setQuantidade(long quantidade) {
+	this.quantidade = quantidade;
+}
+public double getValor() {
+	return valor;
+}
+public void setValor(double valor) {
+	this.valor = valor;
+}
+public Categoria getCategoria() {
+	return categoria;
+}
+public void setCategoria(Categoria categoria) {
+	this.categoria = categoria;
+}
+public Long getCategoriaId() {
+	return categoriaId;
+}
+public void setCategoriaId(Long categoriaId) {
+	this.categoriaId = categoriaId;
+}
+
+
+}

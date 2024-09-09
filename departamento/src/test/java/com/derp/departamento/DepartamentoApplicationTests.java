2d9e3bfc9ac8a98881f03916482b6dd701a4b1b1
caua-labes <caualabesd@gmail.com>
2024-09-09 17:42:45 -0300

    Começo do projeto com mokito

---
+++ departamento/src/test/java/com/derp/departamento/DepartamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derp.departamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
